0b66c748937d8495cc6c2635a7c468f3da743009
Björn Büttner <Idrinth@users.noreply.github.com>
2017-04-28 23:56:39 +0200

    decreasing breaking things in main

--- main.js
+++ main.js
@@ -1,49 +1,82 @@
 var nodegit = require ( "nodegit" );
-
-project = "IDotD/Userscript";
-branch = "idrinth";
-
-require ( "fs-extra" ).remove ( "repository/" + project + "/" + branch, function () {
-    nodegit.Clone ( "https://github.com/" + project, "repository/" + project + "/" + branch, {
-        checkoutBranch: branch,
-        callbacks: {
-            credentials: function () {
-                return nodegit.Cred.userpassPlaintextNew ( "idrinth", "" );
-            }
-        }
-    } )
-            .then ( function ( repo ) {
-                console.log ( "repository/" + project + "/" + branch + '/LICENSE' );
+function sleep(time) {
+    var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        ;
+    }
+}
+var creds = nodegit.Cred.userpassPlaintextNew ( "idrinth", "" );
+var credO = {
+                                callbacks: {
+                                    credentials: function () {
+            console.log("12");
+                                        return creds;
+                                    }
+                                }
+                            };
+project = "Idrinth/automatic-formatting";
+branch = "test";
+var handler = function ( repo ) {
                 while ( !require ( 'fs' ).existsSync ( "repository/" + project + "/" + branch + '/LICENSE' ) ) {
                     sleep ( 1 );
                 }
-                require ( "./src/recursive-format" ) ( "repository/" + project + "/" + branch );
-                repo.getTree ( "HEAD" ).then ( function ( tree ) {
-                    repo.getHeadCommit ().then ( function ( commit ) {
-                        var bot = nodegit.Signature.now ( "Björn Büttner", "Idrinth@users.noreply.github.com" );
-                        nodegit.Commit.create ( repo, "HEAD", bot, bot, "UTF-8", "prettyfiing branch", tree, 1, [ commit ] ).then ( function ( oid ) {
-                            return nodegit.Remote.create ( repo, "origin",
-                                    "git@github.com:" + project + ".git" )
-                                    .then ( function ( remote ) {
-                                        return remote.push (
-                                                [ "refs/heads/" + branch + ":refs/heads/" + branch ],
-                                                {
-                                                    callbacks: {
-                                                        credentials: function () {
-                                                            return nodegit.Cred.userpassPlaintextNew ( "idrinth", "" );
-                                                        }
-                                                    }
-                                                }
-                                        );
-                                    } );
-                        } ).done ( function () {
-                            console.log ( "Done!" );
-                        } );
-                    } );
-                } );
-            } )
-            .catch ( console.log );
-} ).done ( function ( e ) {
-    console.log ( e );
-    console.log ( 'done' );
-} );
+            console.log("23");
+            var Ref;
+                var files = require ( "./src/recursive-format" ) ( "repository/" + project + "/" + branch );
+                console.log(files);
+                    var bot = nodegit.Signature.now ( "Björn Büttner", "Idrinth@users.noreply.github.com" );
+                    repo.createCommitOnHead (
+                        files,
+                        bot,
+                        bot, 
+                        "prettyfiing branch"
+                    ).then ( function ( oid) {
+            console.log("35");
+    console.log("oid "+oid);
+                    return repo.getRemote ( "origin");
+                })
+                .then(function(remote){
+            console.log("39");
+    Ref = remote;
+                    return repo.getBranch(branch);
+                })
+                .then ( function ( remote ) {
+            console.log("43");
+                    return remote.push (
+                            [ Ref.toString () ],
+                            credO
+                    );
+                } )
+                        .then(function(a) {
+                            console.log(a);
+                        });
+            };
+if(require("fs").existsSync("repository/" + project + "/" + branch)) {
+    nodegit.Repository.open ( "repository/" + project + "/" + branch )
+        .then ( function(repo){
+            console.log("68");
+            repo.fetch("origin");
+            return repo;
+        } )
+        .then(function (repo) {
+            console.log("73");
+            repo.mergeBranches(branch, "origin/"+branch);
+            return repo;
+        })
+        .then(handler)
+        .catch ( console.log )
+        .done("pulled and formatted");
+} else {
+    nodegit.Clone ( "https://github.com/" + project, "repository/" + project + "/" + branch, {
+            checkoutBranch: branch,
+            credO
+        } )
+        .then(function(repo){
+            console.log("86");
+            nodegit.Remote.create ( repo, "origin", "git@github.com:" + project + ".git" );
+            return repo;
+        })
+        .then ( handler )
+        .catch ( console.log )
+        .done(function(){console.log("cloned new and formatted");});
+}
--- src/recursive-format.js
+++ src/recursive-format.js
@@ -1,17 +1,30 @@
 fs = require ( "fs" );
 prettier = require ( "prettier" );
+function sleep(time) {
+    var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        ;
+    }
+}
 fm = function ( dir ) {
     var files = fs.readdirSync ( dir );
+    var modified = [];
     for (var pos = files.length - 1; pos >= 0; pos--) {
         var file = dir + '/' + files[pos];
         if ( fs.statSync ( file ).isDirectory () && !file.match ( /\.git$/ ) ) {
-            fm ( file );
+            modified.concat(fm ( file ));
         } else if ( file.match ( /\.js$/ ) ) {
             var content = fs.readFileSync ( file ).toString ();
             if ( !prettier.check ( content ) ) {
                 fs.writeFileSync ( file, prettier.format ( content ) );
+                while(!prettier.check ( fs.readFileSync ( file ).toString () )) {
+                    sleep(2);
+                }
+                modified.push(file);
             }
         }
     }
+    sleep(1);
+    return modified;
 };
 module.exports = fm;
