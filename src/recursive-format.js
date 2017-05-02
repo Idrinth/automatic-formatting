d12c5f55af4521259c70578f222f2761bbab86ea
IdrinthGitbot <IdrinthGitbot@users.noreply.github.com>
2017-05-02 14:11:59 +0200

    prettyfiing branch

--- src/recursive-format.js
+++ src/recursive-format.js
@@ -1,35 +1,36 @@
-fs = require ( "fs" );
-prettier = require ( "prettier" );
+fs = require("fs");
+prettier = require("prettier");
 format = require("./app-config").prettier;
 function sleep(time) {
-    var stop = new Date().getTime();
-    while(new Date().getTime() < stop + time) {
-        ;
-    }
+  var stop = new Date().getTime();
+  while (new Date().getTime() < stop + time) {
+  }
 }
-fm = function ( dir ,base) {
-    base=base?base:dir;
-    require("./if-debug")("formatting:"+dir+" of "+base);
-    var files = fs.readdirSync ( dir );
-    var modified = [];
-    for (var pos = files.length - 1; pos >= 0; pos--) {
-        var file = dir + '/' + files[pos];
-        if ( fs.statSync ( file ).isDirectory () && !file.match ( /\.git$/ ) ) {
-            modified = modified.concat(fm ( file, base ));
-        } else if ( file.match ( /\.js$/ ) ) {
-            var content = fs.readFileSync ( file ).toString ();
-            if ( !prettier.check ( content,format ) ) {
-                content = prettier.format ( content,format);
-                fs.writeFileSync ( file, content );
-                while(!prettier.check ( fs.readFileSync ( file).toString () )) {
-                    sleep(2);
-                }
-                modified.push(file.replace (new RegExp("^"+base.replace(/\//,'\/')+'\/'),''));
-            }
+fm = function(dir, base) {
+  base = base ? base : dir;
+  require("./if-debug")("formatting:" + dir + " of " + base);
+  var files = fs.readdirSync(dir);
+  var modified = [];
+  for (var pos = files.length - 1; pos >= 0; pos--) {
+    var file = dir + "/" + files[pos];
+    if (fs.statSync(file).isDirectory() && !file.match(/\.git$/)) {
+      modified = modified.concat(fm(file, base));
+    } else if (file.match(/\.js$/)) {
+      var content = fs.readFileSync(file).toString();
+      if (!prettier.check(content, format)) {
+        content = prettier.format(content, format);
+        fs.writeFileSync(file, content);
+        while (!prettier.check(fs.readFileSync(file).toString())) {
+          sleep(2);
         }
+        modified.push(
+          file.replace(new RegExp("^" + base.replace(/\//, "\/") + "\/"), "")
+        );
+      }
     }
-    sleep(1);
-    require("./if-debug")("formatted:"+modified.join());
-    return modified;
+  }
+  sleep(1);
+  require("./if-debug")("formatted:" + modified.join());
+  return modified;
 };
 module.exports = fm;
