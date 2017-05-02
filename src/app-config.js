582d8915490e124ba1874963d0712979526f3274
IdrinthGitbot <IdrinthGitbot@users.noreply.github.com>
2017-05-02 14:28:36 +0200

    prettyfiing branch

--- src/app-config.js
+++ src/app-config.js
@@ -1,5 +1,8 @@
-var nodegit =  require("nodegit");
+var nodegit = require("nodegit");
 const setup = JSON.parse(require("fs").readFileSync("config.json").toString());
 module.exports = setup;
-module.exports.creds = nodegit.Cred.userpassPlaintextNew(setup.user.login,setup.user.password);
-module.exports.bot = nodegit.Signature.now(setup.user.name,setup.user.email);
+module.exports.creds = nodegit.Cred.userpassPlaintextNew(
+  setup.user.login,
+  setup.user.password
+);
+module.exports.bot = nodegit.Signature.now(setup.user.name, setup.user.email);
