169f841b9a172524fcc1eb279fb44e6c4690951e
Björn Büttner <idrinth@users.noreply.github.com>
2017-05-08 12:33:56 +0200

    adding defaults

--- src/app-config.js
+++ src/app-config.js
@@ -1,7 +1,35 @@
 var nodegit = require("nodegit");
-const setup = JSON.parse(
+const setup = require("js-object-merge")( {
+    "user": {
+        "login": "",
+        "password": "",
+        "name": "",
+        "email": ""
+    },
+    "port": 33000,
+    "secrets": {},
+    "prettier":{
+        "useTabs": false,
+        "printWidth": 80,
+        "tabWidth": 2,
+        "singleQuote": true,
+        "trailingComma": "none",
+        "bracketSpacing": true,
+        "jsxBracketSameLine": false,
+        "parser": "babylon",
+        "semi": true
+    },
+    "debug": false,
+    "file": {
+        "\\.min\\.js$": false,
+        "\\.js$": true
+    },
+    "directory": {
+        "\\.git": false
+    }
+},JSON.parse(
   require("fs-extra").readFileSync("config.json").toString()
-);
+));
 module.exports = setup;
 module.exports.creds = nodegit.Cred.userpassPlaintextNew(
   setup.user.login,
