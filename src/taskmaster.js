64e233bca48dcd4471b234115718a7e14df2e864
Björn Büttner <Idrinth@users.noreply.github.com>
2017-05-10 10:35:54 +0200

    Update taskmaster.js

--- src/taskmaster.js
+++ src/taskmaster.js
@@ -1,5 +1,11 @@
 var secrets = require("./app-config").secrets;
 var fs = require("fs-extra");
+var crypto = require("crypto");
+var bufferEq = require("buffer-equal-constant-time");
+var debug = require("./if-debug");
+var work = require("./work");
+var timers = require("timers");
+var gitstatus = require("./gitstatus");
 var taskmaster = {
   tasks: {},
   inPr: {},
@@ -8,7 +14,7 @@ var taskmaster = {
     function signBlob(key, blob) {
       return (
         "sha1=" +
-        require("crypto").createHmac("sha1", key).update(blob).digest("hex")
+        crypto.createHmac("sha1", key).update(blob).digest("hex")
       );
     }
     if (!secrets || !secrets[repo]) {
@@ -21,7 +27,7 @@ var taskmaster = {
       return;
     }
     if (
-      !require("buffer-equal-constant-time")(
+      !bufferEq(
         new Buffer(signature),
         new Buffer(signBlob(secrets[repo], body))
       )
@@ -36,7 +42,6 @@ var taskmaster = {
       if (data.ref_type !== "branch") {
         return;
       }
-      var debug = require("./if-debug");
       debug(
         "removing " + "repository/" + data.repository.full_name + "/" + data.ref
       );
@@ -63,7 +68,7 @@ var taskmaster = {
           if (toAdd[c][0] === "pull_request") {
             var name = data.repository.full_name;
             if (data.action !== "closed") {
-              require("./if-debug")(
+              debug(
                 "pulling:" + name + "," + data.pull_request.head.ref
               );
               taskmaster.inPr[name] = taskmaster.inPr[name]
@@ -71,7 +76,7 @@ var taskmaster = {
                 : {};
               taskmaster.inPr[name][data.pull_request.head.ref] = true;
             } else {
-              require("./if-debug")(
+              debug(
                 "not pulling:" + name + "," + data.pull_request.head.ref
               );
               delete taskmaster.inPr[name][data.pull_request.head.ref];
@@ -92,7 +97,7 @@ var taskmaster = {
       var branch = data.ref.split("/")[2];
       if (branch !== "master" && data.head_commit && data.head_commit.id) {
         taskmaster.tasks[name + "|" + branch] = data.head_commit.id;
-        require("./gitstatus").pending(name, data.head_commit.id);
+        gitstatus.pending(name, data.head_commit.id);
       }
     }
     var run = function() {
@@ -104,8 +109,8 @@ var taskmaster = {
           var commit = taskmaster.tasks[id];
           delete taskmaster.tasks[id];
           try {
-            require("./if-debug")("formatting:" + id);
-            require("./work")(id.split("|")[0], id.split("|")[1], commit);
+            debug("formatting:" + id);
+            work(id.split("|")[0], id.split("|")[1], commit);
             return;
           } catch (e) {
             console.log(e);
@@ -114,7 +119,7 @@ var taskmaster = {
       }
     };
     run();
-    require("timers").setTimeout(taskmaster.run, 5000);
+    timers.setTimeout(taskmaster.run, 5000);
   }
 };
 module.exports = taskmaster;
