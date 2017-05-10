6270b92371989e00c65a9707ca77796db48dddd1
Björn Büttner <Idrinth@users.noreply.github.com>
2017-05-10 11:19:45 +0200

    Create if-debug.js

---
+++ test/if-debug.js
@@ -0,0 +1,7 @@
+var should = require('chai').should();
+describe('if-debug', function() {
+  var debug = require('../src/if-debug');
+  it('debug should be a function', function() {
+    config.should.be.a('function');
+  });
+});
