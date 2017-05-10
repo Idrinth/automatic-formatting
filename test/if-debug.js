aa92703c78d1bee4deae29e55992f2ea546070be
Björn Büttner <Idrinth@users.noreply.github.com>
2017-05-10 11:21:12 +0200

    Update if-debug.js

--- test/if-debug.js
+++ test/if-debug.js
@@ -2,6 +2,6 @@ var should = require("chai").should();
 describe("if-debug", function() {
   var debug = require("../src/if-debug");
   it("debug should be a function", function() {
-    config.should.be.a("function");
+    debug.should.be.a("function");
   });
 });
